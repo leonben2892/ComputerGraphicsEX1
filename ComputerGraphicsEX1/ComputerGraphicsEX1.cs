89db7db6db3fe71362aa4b33bc29b5ee04ec9688
Leon <leon.ben2892@gmail.com>
2019-04-06 14:56:43 +0300

    Improved line drawing algorithm to work with all cases

--- ComputerGraphicsEX1/ComputerGraphicsEX1.cs
+++ ComputerGraphicsEX1/ComputerGraphicsEX1.cs
@@ -18,10 +18,10 @@ namespace ComputerGraphicsEX1
         Color color;
 
         //Mouse click points
-        Point firstPoint, secondPoint;
+        Point firstPoint, secondPoint, thirdPoint, fourthPoint;
 
         //Booleans to check if we have 2 points in order to draw line / circle.
-        bool IsPoint1Available, IsPoint2Available;
+        bool IsPoint1Available, IsPoint2Available, IsPoint3Available, IsPoint4Available;
 
         // Booleans to check what we need to draw
         bool IsDrawingLine, IsDrawingCircle, IsDrawingBazierCurve;
@@ -36,7 +36,7 @@ namespace ComputerGraphicsEX1
         {
             bitmapImage = new Bitmap(canvas.Width, canvas.Height);
             color = Color.Black;     
-            IsPoint1Available = IsPoint2Available = false;
+            IsPoint1Available = IsPoint2Available = IsPoint3Available = IsPoint4Available = false;
             IsDrawingLine = true;
             IsDrawingCircle = false;
             IsDrawingBazierCurve = false;
@@ -78,41 +78,145 @@ namespace ComputerGraphicsEX1
 
                 IsPoint2Available = true;
                 if (IsDrawingLine == true)
+                {
                     DrawLine(firstPoint, secondPoint);
+                    IsPoint1Available = false;
+                    IsPoint2Available = false;
+                }                    
                 else if (IsDrawingCircle == true)
+                {
                     DrawCircle(firstPoint, secondPoint);
-
-                IsPoint1Available = false;
-                IsPoint2Available = false;
+                    IsPoint1Available = false;
+                    IsPoint2Available = false;
+                }
             }
-
+            else if(IsPoint3Available == false)
+            {
+                thirdPoint.X = e.X;
+                thirdPoint.Y = e.Y;
+                IsPoint3Available = true;
+            }
+            else
+            {
+                fourthPoint.X = e.X;
+                fourthPoint.Y = e.Y;
+                IsPoint4Available = true;
+            }
             Refresh();
+        }
+
+        private void DrawLineHorizontalSlope(int x, int y, int deltaX, int deltaY, int absDeltaX, int absDeltaY, int endX,int errpX)
+        {
+            // Calculate & draw line points
+            for (int i = 0; x < endX; i++)
+            {
+                x = x + 1;
+                if (errpX < 0)
+                {
+                    errpX += 2 * absDeltaY;
+                }
+                else
+                {
+                    if ((deltaX < 0 && deltaY < 0) || (deltaX > 0 && deltaY > 0))
+                    {
+                        y = y + 1;
+                    }
+                    else
+                    {
+                        y = y - 1;
+                    }
+                    errpX += 2 * (absDeltaY - absDeltaX);
+                }
+                // Draw pixel at (x,y) point
+                bitmapImage.SetPixel(x, y, color);
+            }
+        }
+
+        private void DrawLineVerticalSlope(int x, int y, int deltaX, int deltaY, int absDeltaX, int absDeltaY, int endY, int errpY)
+        {
+            // Calculate & draw line points
+            for (int i = 0; y < endY; i++)
+            {
+                y = y + 1;
+                if (errpY < 0)
+                {
+                    errpY += 2 * absDeltaX;
+                }
+                else
+                {
+                    if ((deltaX < 0 && deltaY < 0) || (deltaX > 0 && deltaY > 0))
+                    {
+                        x = x + 1;
+                    }
+                    else
+                    {
+                        x = x - 1;
+                    }
+                    errpY += 2 * (absDeltaX - absDeltaY);
+                }
+                // Draw pixel at (x,y) point
+                bitmapImage.SetPixel(x, y, color);
+            }
         }
 
         private void DrawLine(Point source, Point target)
         {
+            //Required variables for proper line drawing
+            int x, y, endX, endY;
+
             //Calculating delta X & delta Y
-            int deltaX = Math.Abs(target.X - source.X);
-            int deltaY = Math.Abs(target.Y - source.Y);
+            int deltaX = target.X - source.X;
+            int deltaY = target.Y - source.Y;
 
-            //Calculate Error
-            double err = (deltaY/ deltaX) - 0.5;
-            err += (deltaY / deltaX);
+            //Calculating absolute deltaX & delta Y
+            int absDeltaX = Math.Abs(deltaX);
+            int absDeltaY = Math.Abs(deltaY);
 
-            //Improve algorithm to use only integers
-            int errp = Convert.ToInt32(2 * deltaX * err);
+            //Calculate error for both axis in order to be able to draw lines with horizontal & vertical slope
+            int errpX = 2 * absDeltaY - absDeltaX;
+            int errpY = 2 * absDeltaX - absDeltaY;
 
-            //Draw line points
-            for (int x = source.X, y = source.Y; x <= target.X; x++)
+            //Bigger slope on X axis
+            if (absDeltaX > absDeltaY)
             {
-                bitmapImage.SetPixel(x, y, color);
-                if(errp >= 0)
+                //Line is left to right
+                if (deltaX >= 0)
                 {
-                    y++;
-                    errp -=  2 * deltaX;
+                    x = source.X;
+                    y = source.Y;
+                    endX = target.X;
+                }
+                //Line is right to left
+                else
+                {
+                    x = target.X;
+                    y = target.Y;
+                    endX = source.X;
                 }
 
-                errp += 2 * deltaY;
+                //Calculate & draw line points
+                DrawLineHorizontalSlope(x, y, deltaX, deltaY, absDeltaX, absDeltaY, endX, errpX);
+            }
+            //Bigger slope on Y axis
+            else
+            {
+                //Line is bottom to top
+                if (deltaY >= 0)
+                {
+                    x = source.X;
+                    y = source.Y;
+                    endY = target.Y;
+                }
+                //Line is top to bottom
+                else
+                {
+                    x = target.X;
+                    y = target.Y;
+                    endY = source.Y;
+                }
+
+                //Calculate & draw line points
+                DrawLineVerticalSlope(x, y, deltaX, deltaY, absDeltaX, absDeltaY, endY, errpY);
             }
         }
 
